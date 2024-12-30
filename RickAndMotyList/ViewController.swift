967ea20299cfabc7c38a9702c2892daba4f2d6e9
Gaspar Dolcemascolo <gdolcemascolo@falabella.cl>
2024-12-30 15:20:15 -0300

    Initial Commit

---
+++ RickAndMotyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMotyList
+//
+//  Created by Gaspar Dolcemascolo on 30-12-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
